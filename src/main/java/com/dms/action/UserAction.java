b7f7c25610a79c747b9c15b8d4bad6dd5d0b74d2
BrightSt <1402252869@qq.com>
2018-05-21 11:01:30 +0800

    c

---
+++ src/main/java/com/dms/action/UserAction.java
@@ -0,0 +1,43 @@
+package com.dms.action;
+
+import com.dms.entity.User;
+import com.dms.service.UserService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * Created by kinthon on 17-6-25.
+ */
+
+@Controller("userAction")
+@Scope("prototype")
+public class UserAction extends ActionSupport {
+    @Resource
+    private UserService userService;
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String login() {
+        if(userService.login(user)) {
+            Map session = ActionContext.getContext().getSession();
+            session.put("user", user);
+            return SUCCESS;
+        } else {
+            return ERROR;
+        }
+    }
+
+}
---
+++ src/main/webapp/index1.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Dong
+  Date: 2018/5/21
+  Time: 10:08
+  To change this template use File | Settings | File Templates.
+--%>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+666666
+</body>
+</html>
